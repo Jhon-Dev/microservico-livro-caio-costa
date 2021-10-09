19755202200d4ef7a3e3cf26848e6ce2f330df40
jhonatan Nuss <43417699+Jhon-Dev@users.noreply.github.com>
2021-10-09 00:04:10 -0300

    Criando projeto e configurando com o banco

---
+++ src/main/java/Microsservico/para/gestao/de/tarefas/TaskApplication.java
@@ -0,0 +1,13 @@
+package Microsservico.para.gestao.de.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApplication.class, args);
+	}
+
+}
